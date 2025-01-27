e057dac2512ff6b6bc3c621ae321bc01cb37bfa2
loremdev2 <singanamalaaman@gmail.com>
2025-01-28 00:37:21 +0530

    Removed Theme Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 import { useState, useEffect } from "react";
-import { ModeToggle } from "./components/ThemeProvider/theme-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-import Image from "next/image";
-import InputWidget from "./components/Input/InputWidget";
-import { useCallback, useState } from "react";
 
 export default function Home() {
   
